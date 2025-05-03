fca9abf6c6474caaa3585355b84c5784d841e1fd
humanshield85 <rachidboudjelida@gmail.com>
2025-05-03 02:31:16 +0100

    v 1.0

--- src/pages/gtav.jsx
+++ src/pages/gtav.jsx
@@ -50,16 +50,23 @@ function Gtav() {
       <div className="gtav-carousel card">
         <CustomCarCarousel />
       </div>
+      <div className="more-div card ">
+        <h2>
+          Experience the thrill of real-life car mods — now rolling into your
+          GTA garage in stunning detail.
+        </h2>
+        <button>View Collection</button>
+      </div>
       <div class="cards-container">
         <div class="card1 card ">
           <div className="card-content">
             {" "}
             <div className="card-text">
-              <h1>Angel</h1>
-              <p>Top speed : 190.00 km/h</p>
+              <h1>Penaud La Coureuse</h1>
+              <p>Top speed : 184.27 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img21 ci"></div>
+            <div className="card-img41 ci"></div>
           </div>
         </div>
         <div class="card2 card">
@@ -67,11 +74,11 @@ function Gtav() {
             {" "}
             <div className="card-text">
               {" "}
-              <h1>Blista Compact</h1>
-              <p>Top speed : 200.00 km/h</p>
+              <h1>Declasse Vigero ZX</h1>
+              <p>Top speed : 201.17 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img22 ci"></div>
+            <div className="card-img42 ci"></div>
           </div>
         </div>
         <div class="card3 card">
@@ -79,11 +86,11 @@ function Gtav() {
             {" "}
             <div className="card-text">
               {" "}
-              <h1>Bloodring Banger Glendale</h1>
-              <p>Top speed : 160.00 km/h</p>
+              <h1>Baller</h1>
+              <p>Top speed : 180.25 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img23 ci"></div>
+            <div className="card-img43 ci"></div>
           </div>
         </div>
         <div class="card4 card">
@@ -91,11 +98,11 @@ function Gtav() {
             {" "}
             <div className="card-text">
               {" "}
-              <h1>Cuban Hermes</h1>
-              <p>Top speed : 160.00 km/h</p>
+              <h1>Obey 10F</h1>
+              <p>Top speed : 202.78 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img24 ci"></div>
+            <div className="card-img44 ci"></div>
           </div>
         </div>
         <div class="card5 card">
@@ -103,11 +110,11 @@ function Gtav() {
             {" "}
             <div className="card-text">
               {" "}
-              <h1>Deluxo</h1>
+              <h1>Nagasaki Shinobi</h1>
               <p>Top speed : 200.00 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img25 ci"></div>
+            <div className="card-img45 ci"></div>
           </div>
         </div>
         <div class="card6 card">
@@ -115,11 +122,11 @@ function Gtav() {
             {" "}
             <div className="card-text">
               {" "}
-              <h1>Gang Burrito</h1>
-              <p>Top speed : 150.00 km/h</p>
+              <h1>Growler</h1>
+              <p>Top speed : 220.00 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img26 ci"></div>
+            <div className="card-img46 ci"></div>
           </div>
         </div>
         <div class="card7 card">
@@ -127,44 +134,44 @@ function Gtav() {
             {" "}
             <div className="card-text">
               {" "}
-              <h1>Hotring Racer</h1>
-              <p>Top speed : 220.00 km/h</p>
+              <h1>Brioso 300</h1>
+              <p>Top speed : 140.00 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img27 ci"></div>
+            <div className="card-img47 ci"></div>
           </div>
         </div>
         <div class="card8 card">
           <div className="card-content">
             <div className="card-text">
               {" "}
-              <h1>Maverick</h1>
+              <h1>Youga 4x4</h1>
               <p>Top speed : 200.00 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img28 ci"></div>
+            <div className="card-img48 ci"></div>
           </div>
         </div>
         <div class="card9 card">
           <div className="card-content">
             <div className="card-text">
               {" "}
-              <h1>Sabre Turbo</h1>
-              <p>Top speed : 180.00 km/h</p>
+              <h1>Jester RR</h1>
+              <p>Top speed : 200.00 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img29 ci"></div>
+            <div className="card-img49 ci"></div>
           </div>
         </div>
         <div class="card10 card">
           <div className="card-content">
             <div className="card-text">
               {" "}
-              <h1>Stallion</h1>
-              <p>Top speed : 160.00 km/h</p>
+              <h1>Grotti Vigilante</h1>
+              <p>Top speed : 236.17 km/h</p>
               <button>More info</button>
             </div>
-            <div className="card-img30 ci"></div>
+            <div className="card-img50 ci"></div>
           </div>
         </div>
       </div>
